c8c2a48e4de818522492ff4219df50247c7f0bf5
fernandoberbel <fberbel1000@gmail.com>
2025-02-26 00:32:07 -0300

    feat: add initial home screen

---
+++ src/screens/home/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Alert,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import icons from "../../constants/icons.js";
+import { styles } from "./styles.js";
+
+export default function Home() {
+  function OpenPassenger() {
+    Alert.alert("Abrir tela de passageiros...");
+    navi;
+  }
+  function OpenRide() {
+    Alert.alert("Abrir tela de motoristas...");
+  }
+
+  return (
+    <ImageBackground source={icons.bg} resizeMode="cover" style={styles.bg}>
+      <Image source={icons.logo} style={styles.logo} />
+      <TouchableOpacity style={styles.btn} onPress={OpenPassenger}>
+        <Image source={icons.passenger} style={styles.img} />
+        <Text style={styles.title}>Passageiro</Text>
+        <Text style={styles.text}>Encontre uma carona para você.</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn} onPress={OpenRide}>
+        <Image source={icons.driver} style={styles.img} />
+        <Text style={styles.title}>Motorista</Text>
+        <Text style={styles.text}>Ofereça carona em seu carro.</Text>
+      </TouchableOpacity>
+    </ImageBackground>
+  );
+}
